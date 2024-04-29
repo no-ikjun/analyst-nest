527a4de215a2bae8257e4e729b044942f40b2d69
ikjunchoi <choiikjun1101@gmail.com>
2024-04-29 11:44:36 +0900

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,32 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class AppService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async fetchApiData(query: string) {
+    const serviceKey = this.configService.get<string>('SERVICE_KEY');
+    try {
+      const response = await firstValueFrom(
+        this.httpService.get(
+          `https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=${serviceKey}&numOfRows=5&pageNo=1&resultType=json&likeItmsNm=${query}`,
+        ),
+      );
+      return response.data;
+    } catch (error) {
+      // 에러 핸들링
+      console.error(error);
+      throw new Error('API request failed');
+    }
+  }
+}
