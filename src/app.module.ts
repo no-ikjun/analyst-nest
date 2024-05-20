d0efb360ece671f44f10bed7ffec06cad12e2290
ikjunchoi <choiikjun1101@gmail.com>
2024-05-20 09:03:58 +0900

    🎨 update: database setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GptModule } from './gpt/gpt.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './global/config/database/database.module';
+import { DatabaseService } from './global/config/database/database.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     HttpModule,
     ConfigModule.forRoot({
+      cache: true,
       isGlobal: true,
-      envFilePath: `.env`,
+      envFilePath: '.env',
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [DatabaseModule],
+      useClass: DatabaseService,
+      inject: [DatabaseService],
+    }),
+    DatabaseModule,
     GptModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService, ConfigService],
 })
 export class AppModule {}
---
+++ src/global/config/database/database.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      username: this.configService.get<string>('DATABASE_USER'),
+      password: this.configService.get<string>('DATABASE_PASSWORD'),
+      port: this.configService.get<number>('DATABASE_PORT'),
+      host: this.configService.get<string>('DATABASE_HOST'),
+      database: this.configService.get<string>('DATABASE_NAME'),
+      timezone: '+09:00',
+      entities: [],
+      synchronize: true,
+    };
+  }
+}
