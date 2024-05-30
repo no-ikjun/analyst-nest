f01221f4a145c528097257c06c9d48f9558b5a12
ikjunchoi <choiikjun1101@gmail.com>
2024-05-30 15:56:32 +0900

    add guard to message controller

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Get, Post, Query, Req } from '@nestjs/common';
+import { Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { MessageService } from './message.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('message')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   async getMessage(@Query('userId') userId: number) {
     return this.messageService.findMessageByUserId(userId);
   }
 
   @Post()
-  async setMessage(@Query('url') url: string, @Req() req) {
-    const accessToken = req.headers.authorization.split(' ')[1];
-    return this.messageService.setMessage(accessToken, url);
+  async setMessage(@Req() request, @Query('url') url: string) {
+    const token = request.headers.authorization.split(' ')[1];
+    return this.messageService.setMessage(token, url);
   }
 }
