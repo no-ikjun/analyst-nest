8bc7c92c9b8f86873b47a1314b8d367d777d3d32
ikjunchoi <choiikjun1101@gmail.com>
2024-05-30 23:37:06 +0900

    test gpt report

--- src/gpt/gpt.module.ts
+++ src/gpt/gpt.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { GptService } from './gpt.service';
 import { GptController } from './gpt.controller';
+import { TaskService } from 'src/task/task.service';
+import { KisService } from 'src/kis/kis.service';
+import { MessageService } from 'src/message/message.service';
+import { UserService } from 'src/user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { KisToken } from 'src/global/entities/kistoken.entity';
+import { Interest } from 'src/global/entities/interest.entity';
+import { Message } from 'src/global/entities/message.entity';
+import { ForeignInterest } from 'src/global/entities/foreignInterest.entity';
+import { HttpModule } from '@nestjs/axios';
+import { UserRepository } from 'src/user/user.repository';
 
 @Module({
-  providers: [GptService],
-  controllers: [GptController]
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([KisToken, Interest, Message, ForeignInterest]),
+  ],
+  providers: [
+    GptService,
+    TaskService,
+    KisService,
+    MessageService,
+    UserService,
+    UserRepository,
+  ],
+  controllers: [GptController],
 })
 export class GptModule {}
