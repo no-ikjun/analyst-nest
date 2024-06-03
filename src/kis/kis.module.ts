c954b433535c8f99082cfd9a28150fcd70eb894b
ikjunchoi <choiikjun1101@gmail.com>
2024-06-03 10:38:22 +0900

    add realtime messaging endpoint

--- src/kis/kis.module.ts
+++ src/kis/kis.module.ts
@@ -6,13 +6,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { KisToken } from 'src/global/entities/kistoken.entity';
 import { Interest } from 'src/global/entities/interest.entity';
 import { ForeignInterest } from 'src/global/entities/foreignInterest.entity';
+import { TaskService } from 'src/task/task.service';
+import { UserService } from 'src/user/user.service';
+import { MessageService } from 'src/message/message.service';
+import { GptService } from 'src/gpt/gpt.service';
+import { UserRepository } from 'src/user/user.repository';
+import { Message } from 'src/global/entities/message.entity';
 
 @Module({
   imports: [
     HttpModule,
-    TypeOrmModule.forFeature([KisToken, Interest, ForeignInterest]),
+    TypeOrmModule.forFeature([KisToken, Interest, ForeignInterest, Message]),
   ],
   controllers: [KisController],
-  providers: [KisService],
+  providers: [
+    KisService,
+    TaskService,
+    UserService,
+    MessageService,
+    GptService,
+    UserRepository,
+  ],
 })
 export class KisModule {}
